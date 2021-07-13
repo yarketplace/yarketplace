e73d44742cdda6eab8335606b0f73fe0c834d4c4
Kyle Andruczk <kyle.andruczk@gmail.com>
2021-07-13 19:41:06 -0400

    built out CR for posts

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -3,4 +3,30 @@ class PostsController < ApplicationController
     def index
         @posts = Post.all
     end
+
+    def show
+        @post = Post.find_by(id: params[:id])
+    end
+
+    def new
+        @post = Post.new
+        @users = User.all
+    end
+
+    def create
+        @post = Post.create(post_params)
+        
+        if @post.valid?
+            redirect_to post_path(@post)
+        else
+            # add error display
+            redirect_to new_post_path(@post)
+        end
+    end
+
+    private
+
+    def post_params
+        params.require(:post).permit(:user_id, :title, :category, :price, :desc)
+    end
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,16 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+
+10.times do 
+    User.create(name: Faker::Name.name, location: Faker::Address.city, address: Faker::Address.street_address, zip_code: Faker::Address.zip, phone_number: rand(1111111111..9999999999), email: Faker::Internet.email)
+end
+
+10.times do 
+    Post.create(user_id: User.ids.sample, title: Faker::GreekPhilosophers.quote, category: Post.categories.sample, price: rand(0.01..99.99), desc: Faker::Hacker.say_something_smart)
+end
+
+10.times do 
+    Comment.create(user_id: User.ids.sample, post_id: Post.ids.sample, content: Faker::ChuckNorris.fact )
+end
