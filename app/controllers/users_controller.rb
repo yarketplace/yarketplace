391cf1e394906535410424212fbdf7585866c1ae
Anh Dang <anhcdang@gmail.com>
2021-07-13 14:22:57 -0700

    create UsersController, create signup routes

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,23 @@
 class UsersController < ApplicationController
+    def new
+        @user = User.new
+    end
+
+    def create
+        user = User.new(user_params)
+
+        if user.save
+            session[:user_id] = @user.id
+            # --> redirect to home page?
+        else
+            # --> error message handling
+            render :new 
+        end
+    end
+
+    private
+
+    def user_params
+        params.require(:user).permit(:name, :location, :address, :zip_code, :phone_number, :email)
+    end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,8 @@ Rails.application.routes.draw do
   resources :comments
   resources :posts
   resources :users
+  get '/signup', to: 'users#new'
+  post '/signup-attempt', to: 'users#create'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   resources :images, only: [:index, :show, :new, :create]
   root 'images#new'
