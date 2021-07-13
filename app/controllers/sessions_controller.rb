fed419319cdd704e25ec9995b3eedfe0fabd6083
Anh Dang <anhcdang@gmail.com>
2021-07-13 14:51:32 -0700

    create sign in form

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -11,5 +11,5 @@ class SessionsController < ApplicationController
 
     def destroy
         session[:user_id] = nil
-      end      
+    end      
 end
--- config/routes.rb
+++ config/routes.rb
@@ -8,7 +8,7 @@ Rails.application.routes.draw do
 
   get '/signin', to: 'sessions#new'
   post '/signin', to: 'sessions#create'
-  destroy '/signout', to: 'sessions#destroy'
+  delete '/signout', to: 'sessions#destroy'
 
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   resources :images, only: [:index, :show, :new, :create]
