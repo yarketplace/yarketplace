94f8b2b8114ceae28f06c909d1c817305eb2f49f
Kyle Andruczk <kyle.andruczk@gmail.com>
2021-07-13 12:04:05 -0400

    Added relationships to user and post

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,5 @@
 class Post < ApplicationRecord
+    belongs_to :poster, :foriegn_key => :poster_id, class_name: 'User'
+
+    has_many :comments
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,5 @@
 class User < ApplicationRecord
+
+
+    has_many :posts, :foriegn_key => :poster_id
 end
