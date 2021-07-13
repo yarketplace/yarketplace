dd6f02663e05dffa222e5cbde0814f5123bc821e
Anh Dang <anhcdang@gmail.com>
2021-07-13 11:20:29 -0700

    ActiveStorage setup, Image model/controller setup

---
+++ app/controllers/images_controller.rb
@@ -0,0 +1,30 @@
+class ImagesController < ApplicationController
+    def index
+        @images = Image.all
+    end
+
+    def show
+        @image = Image.find_by(id: params[:id])
+    end
+
+    def new
+        @image = Image.new
+    end
+
+    def create
+        image = Image.new(image_params)
+        
+        if image.save
+            redirect_to image_path(image)
+        else
+            # add error display
+            render :new
+        end
+    end
+
+    private
+
+    def image_params
+        params.require(:image).permit(:uploaded_image)
+    end
+end
