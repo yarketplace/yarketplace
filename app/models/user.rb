44920ced9908c434cf39969e2c082184c8830ba5
Anh Dang <anhcdang@gmail.com>
2021-07-14 13:19:56 -0700

    tidy up signin/signup form

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,4 +4,26 @@ class User < ApplicationRecord
     has_many :comments
 
     has_secure_password
+
+    def location_options
+        [
+            ["East Rock"], # what other off-campus areas?
+            ["Yale College: Old Campus", "Old Campus"],
+            ["Yale College: Benjamin Franklin", "Benjamin Franklin"],
+            ["Yale College: Berkeley", "Berkeley"],
+            ["Yale College: Branford", "Branford"],
+            ["Yale College: Davenport", "Davenport"],
+            ["Yale College: Ezra Stiles", "Ezra Stiles"],
+            ["Yale College: Grace Hopper", "Grace Hopper"],
+            ["Yale College: Jonathan Edwards", "Jonathan Edwards"],
+            ["Yale College: Morse", "Morse"],
+            ["Yale College: Pauli Murray", "Pauli Murray"],
+            ["Yale College: Pierson", "Pierson"],
+            ["Yale College: Saybrook", "Saybrook"],
+            ["Yale College: Silliman", "Silliman"],
+            ["Yale College: Timothy Dwight", "Timothy Dwight"],
+            ["Yale College: Trumball", "Trumball"],
+            ["Other"]
+        ]
+    end
 end
