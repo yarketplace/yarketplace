1ca3c51b5c17183c1c7df39fc492034e0d246ef6
Kyle Andruczk <kyle.andruczk@gmail.com>
2021-07-14 18:39:30 -0400

    trying to fix images in post form

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,6 +1,7 @@
 class Post < ApplicationRecord
     has_many :comments, dependent: :destroy
     belongs_to :user
+    has_many :images, dependent: :destroy
 
     # creates a global array of acceptable categories
     def self.categories
