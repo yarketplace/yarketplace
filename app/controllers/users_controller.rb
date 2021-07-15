9d9dd2f0753fd288b7079fce1654ba0527bebd4f
Taek Jin Han <tj.han@yale.edu>
2021-07-15 15:05:10 -0400

    testing

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -15,7 +15,7 @@ class UsersController < ApplicationController
         @user = User.new(user_params)
 
         if @user.save
-            session[:user_id] = user.id
+            session[:user_id] = @user.id
             redirect_to home_path
         else
             # --> error message handling
